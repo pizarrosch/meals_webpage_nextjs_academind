e3a56bea4faf93af9dd4b6d0dcbb9cd938cccf24
pizarrosch <bayernsch89@gmail.com>
2025-01-10 14:06:57 +0100

    Commit #3: Move the header bg to a separate component

---
+++ app/components/main-header/main-header.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import logoImg from '@/assets/logo.png';
+import s from './main-header.module.css';
+import Image from "next/image";
+import MainHeaderBackground from "@/app/components/main-header/main-header-background";
+
+export default function MainHeader() {
+    return (
+        <>
+            <MainHeaderBackground />
+            <header className={s.header}>
+                <Link className={s.logo} href='/public'>
+                    <Image src={logoImg} alt='A plate with food' priority/>
+                    NextLevel Food
+                </Link>
+                <nav className={s.nav}>
+                    <ul>
+                        <li><Link href='/meals'>Browse Meals</Link></li>
+                        <li><Link href='/community'>Foodies Community</Link></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+
+    )
+}
