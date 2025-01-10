3251ba76b9aecff391e3899ea813ad0241b250fd
pizarrosch <bayernsch89@gmail.com>
2025-01-10 18:11:18 +0100

    Commit #6: Highlight the title of the open page using usePathname hook

--- app/components/main-header/main-header.js
+++ app/components/main-header/main-header.js
@@ -1,10 +1,16 @@
+'use client'
+
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 import logoImg from '@/assets/logo.png';
 import s from './main-header.module.css';
 import Image from "next/image";
 import MainHeaderBackground from "@/app/components/main-header/main-header-background";
 
 export default function MainHeader() {
+  
+  const path = usePathname();
+  
   return (
     <>
       <MainHeaderBackground/>
@@ -15,8 +21,8 @@ export default function MainHeader() {
         </Link>
         <nav className={s.nav}>
           <ul>
-            <li><Link href='/meals'>Browse Meals</Link></li>
-            <li><Link href='/community'>Foodies Community</Link></li>
+            <li><Link href='/meals' className={path.startsWith('/meals') && s.active}>Browse Meals</Link></li>
+            <li><Link href='/community' className={path === '/community' && s.active}>Foodies Community</Link></li>
           </ul>
         </nav>
       </header>
