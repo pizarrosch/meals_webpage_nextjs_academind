34734f3bfcca16f87c9c0e24b238270090114304
pizarrosch <bayernsch89@gmail.com>
2025-03-01 15:30:49 +0100

    Commit #13: Create a "not found" page in case a user types in the wrong address

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Unfortunately, we could not find the requested page. Please check whether you typed in the right page address</p>
+    </main>
+  )
+}
