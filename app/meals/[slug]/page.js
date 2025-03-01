39a790d0699c7839367c294c10c2db231273e0ce
pizarrosch <bayernsch89@gmail.com>
2025-03-01 16:06:16 +0100

    Commit #14: Display meal details

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,11 +1,32 @@
 import Link from "next/link";
+import Image from "next/image";
+import s from './page.module.css';
+import {getMeal} from "@/lib/meals";
 
 export default async function MealDetailsPage({params}) {
   
+  const meal = getMeal(params.slug);
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+  
   return (
-    <main>
-      <h1>{details}</h1>
-      <p><Link href='../../meals'>Back to meals</Link></p>
-    </main>
+    <>
+      <header className={s.header}>
+        <div className={s.image}>
+          <Image src={meal.image} alt={meal.title} fill/>
+        </div>
+        <div className={s.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={s.creator}>
+            by <a href={`mailto: ${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={s.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={s.instructions} dangerouslySetInnerHTML={{
+          __html: meal.instructions,
+        }}></p>
+      </main>
+    </>
   )
 }
