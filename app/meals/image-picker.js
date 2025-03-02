92d86c1df17c051e6422229224a31f0153dd310f
pizarrosch <bayernsch89@gmail.com>
2025-03-02 23:42:17 +0100

    Commit #17: Show preview of the selected image
    Add image to the state as a URL address, which can be read via built-in FileReader class (new FileReader()).

--- app/meals/image-picker.js
+++ app/meals/image-picker.js
@@ -1,10 +1,31 @@
 'use client'
 
 import s from './image-picker.module.css';
-import {useRef} from "react";
+import {useRef, useState} from "react";
+import Image from 'next/image';
 
 export default function ImagePicker({label, name}) {
+  
+  const [pickedImage, setPickedImage] = useState(null);
+  
+  function handleImageChange(e) {
+    const file = e.target.files[0];
+    
+    if(!file) {
+      setPickedImage(null);
+    }
+    
+    const fileReader = new FileReader();
+    
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    }
+    
+    fileReader.readAsDataURL(file);
+  }
+  
   const imageInput = useRef();
+  
   function handlePickClick() {
     imageInput.current.click();
   }
@@ -13,7 +34,12 @@ export default function ImagePicker({label, name}) {
     <div className={s.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={s.controls}>
-        <input className={s.input} type='file' id={name} name={name} accept='image/phg, image/jpeg' ref={imageInput}/>
+        <div className={s.preview}>
+          {!pickedImage && <p>No image picked yet</p>}
+          {pickedImage && <Image src={pickedImage} alt='My image' fill />}
+        </div>
+        <input className={s.input} type='file' id={name} name={name} accept='image/phg, image/jpeg' ref={imageInput}
+               onChange={handleImageChange} required />
         <button className={s.button} type='button' onClick={handlePickClick}>Pick an image</button>
       </div>
     </div>
